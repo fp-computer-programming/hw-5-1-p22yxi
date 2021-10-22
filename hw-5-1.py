dd9feef2436b4eafeed1d0c206d712083dc921d6
p22yxi <p22yxi@fairfieldprep.org>
2021-10-22 09:59:14 -0400

    unfinished

---
+++ hw-5-1.py
@@ -0,0 +1,20 @@
+# Author Ypngdong Xi (Oct 22 2021)
+
+import random
+# Question 1
+print(random.randrange(30, 50, 1))
+
+# Question 2
+print(random.randrange(2, 75, 2))
+
+# Question 3
+print(random.randint(20, 30))
+
+# Question 4
+print(random.randint(1, 8))
+
+# Question 5
+print(random.randint(1, 20))
+
+# Question 6
+print(random.choice('cat', 'dog', 'sheep', 'cow', 'chicken', 'pig'))
