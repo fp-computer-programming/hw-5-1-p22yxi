5254451499a2e65e71f8125d555c8d36e48eacb1
p22yxi <p22yxi@fairfieldprep.org>
2021-10-29 11:10:04 -0400

    maybe unfinished

--- hw-5-1.py
+++ hw-5-1.py
@@ -17,4 +17,22 @@ print(random.randint(1, 8))
 print(random.randint(1, 20))
 
 # Question 6
-print(random.choice('cat', 'dog', 'sheep', 'cow', 'chicken', 'pig'))
+seq = 'cat', 'dog', 'sheep', 'cow', 'chicken', 'pig'
+print(random.choice(seq))
+
+# Question 7
+population = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(random.choices(population, k=4))
+
+# Question 8
+population2 = [1, 2, 3, 4, 5]
+print(random.sample(population2, k=5))
+
+# Question 9
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
+card = random.choice(cards)
+print("the card you drawed was", card)
+
+# Question 10
+random.seed(1942)
+print(random.randint(1, 1000))
